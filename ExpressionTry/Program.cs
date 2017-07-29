e8878df10e54d6636d59f8fdd90d30a35a25b14c
Hoken  Guan <h2020856@hotmail.com>
2017-07-29 15:48:10 +0800

    initial    2

--- ExpressionTry/Program.cs
+++ ExpressionTry/Program.cs
@@ -19,18 +19,16 @@ namespace ExpressionTry
             //var lambda = Expression.Lambda<Func<IEnumerable<String>, IEnumerable<String>>>(orderByExp, list);
             //var data = new String[] { "asdasdasd", "asdads", "123", "xcvxcvs", "ASDSD" };
             //var result = lambda.Compile()(data);
-
+            Expression<Func<Outer, int>> exp = o => o.B;
             List<Outer> items = new List<Outer> { new Outer { B = 1, InnerObj = new Inner { A = 2 } }, new Outer { B = 3, InnerObj = new Inner { A = 4 } }, new Outer { B = 3, InnerObj = new Inner { A = 6 } } };
-
-            Expression ex_x = Expression.Parameter(typeof (Outer), "x");
-            Expression ex_y = Expression.Parameter(typeof (Outer), "y");
-            Expression ex_Bofx = Expression.PropertyOrField(ex_x, "B");
-            
+            var list = Expression.Parameter(typeof(IEnumerable<Outer>), "list");
+            //Console.Write(exp.Body.Type);
             //var method = typeof (Enumerable).GetMethods().First(x => x.Name == "OrderByDescending");
-            Expression final = Expression.Call(typeof(Enumerable),"OrderBy", new []{typeof(Outer),typeof(int)}, ex_x, ex_Bofx );
+            Expression final = Expression.Call(typeof(Enumerable),"OrderByDescending", new []{typeof(Outer),typeof(int)},list, exp );
+            var lambda = Expression.Lambda<Func<IEnumerable<Outer>,IEnumerable<Outer>>>(final, list);
             items.OrderByDescending<Outer,int>(x=>x.B);
 
-            foreach (var aaa in items.AsQueryable().Provider.CreateQuery<Outer>(final))
+            foreach (var aaa in lambda.Compile()(items))
             {
                 Console.WriteLine(aaa.B + ";" + aaa.InnerObj.A);
             }
